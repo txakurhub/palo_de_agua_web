317dca5be49d8ae94813c1b87df12e2f4a0102bc
unknown <me.leandrop@gmail.com>
2023-05-27 17:13:48 -0300

    tailwind cfg

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        celeste: "#1B82C5",
+      },
+    },
+  },
+  plugins: [],
+};
