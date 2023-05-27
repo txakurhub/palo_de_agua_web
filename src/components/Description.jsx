0dd0b884ce742b0610645bbe2e89e63ba59ee178
unknown <me.leandrop@gmail.com>
2023-05-27 17:14:15 -0300

    description sec

---
+++ src/components/Description.jsx
@@ -0,0 +1,21 @@
+const Description = () => {
+  return (
+   
+      <h3 style={{ opacity: 1 }} className="text-lg font-semibold text-center py-10">
+        Palo de Agua nace entre la necesidad de revalorizar nuestras creaciones,
+        y el contagio de aquella alegre rebeldía. Es un encuentro que hizo
+        confluir caminos, y así logramos fundir la producción de poesías y
+        universos musicales. Desde el sur de México, desde cada rinconcito del
+        continente, Palo de Agua crece como un colectivo de música en canciones,
+        para seguir compartiendo el viaje. Es un proyecto que nace en San
+        Cristóbal de las Casas, Chiapas, México, pero sus orígenes están
+        nutridos por años de búsqueda constante. Procurando en cada territorio
+        maestros y maestras como fuente de conocimiento, viajar por
+        latinoamérica se volvió una escuela de artes, que está presente en cada
+        canción.
+      </h3>
+  
+  );
+};
+
+export default Description;
